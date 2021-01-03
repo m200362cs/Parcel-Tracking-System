8517048449f610e2622216ec507106812757bad3
m200362cs <76104308+m200362cs@users.noreply.github.com>
2021-01-03 18:30:50 +0530

    phone number validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_01_105536) do
+ActiveRecord::Schema.define(version: 2021_01_03_064733) do
 
   create_table "consignments", force: :cascade do |t|
     t.integer "destination_pin"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_01_01_105536) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "Phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
